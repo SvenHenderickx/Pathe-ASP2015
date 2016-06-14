1890432f5b9b5439b8434b1ccd75829775847a71
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-06-14 09:45:08 +0200

    review

---
+++ Pathe-ASP2015Tests/Models/HandlerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pathe_ASP2015.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathe_ASP2015.Models.Tests
+{
+    [TestClass()]
+    public class HandlerTests
+    {
+        [TestMethod()]
+        public void GetBioscopenTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
